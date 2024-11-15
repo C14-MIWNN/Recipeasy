8961218083dcf5b3649252b5f3eef76b65e68323
Johannes <jk.wierenga@st.hanze.nl>
2024-11-15 14:07:23 +0100

    Add RecipeRepository.

---
+++ src/main/java/nl/miwnn/se14/ares/recipeasy/repositories/RecipeRepository.java
@@ -0,0 +1,10 @@
+package nl.miwnn.se14.ares.recipeasy.repositories;
+
+import nl.miwnn.se14.ares.recipeasy.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    Optional<Recipe> findByName(String name);
+}
