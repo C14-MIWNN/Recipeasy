17195460a41ed8a0daf44c5e116a4edb9006ee45
Johannes <jk.wierenga@st.hanze.nl>
2024-11-18 16:28:53 +0100

    Clean up IngredientRepository (remove JavaDoc and unused import).

--- src/main/java/nl/miwnn/se14/ares/recipeasy/repositories/IngredientRepository.java
+++ src/main/java/nl/miwnn/se14/ares/recipeasy/repositories/IngredientRepository.java
@@ -2,14 +2,9 @@ package nl.miwnn.se14.ares.recipeasy.repositories;
 
 import nl.miwnn.se14.ares.recipeasy.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-/**
- * @author armazadev
- * Purpose
- */
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
     Optional<Ingredient> findByName(String name);
 }
