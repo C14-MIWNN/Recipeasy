75ea8fa84a6fec35faa6f506b975b602a2202bc6
armazade <armantje2001@gmail.com>
2024-11-26 11:04:44 +0100

    homepage add button fix
    right fragment path and give right arguments in controller

--- src/main/java/nl/miwnn/se14/ares/recipeasy/controller/RecipeController.java
+++ src/main/java/nl/miwnn/se14/ares/recipeasy/controller/RecipeController.java
@@ -1,6 +1,7 @@
 package nl.miwnn.se14.ares.recipeasy.controller;
 
 import nl.miwnn.se14.ares.recipeasy.dto.RecipeUserDTO;
+import nl.miwnn.se14.ares.recipeasy.model.CuisineType;
 import nl.miwnn.se14.ares.recipeasy.model.Recipe;
 import nl.miwnn.se14.ares.recipeasy.model.RecipeUser;
 import nl.miwnn.se14.ares.recipeasy.repositories.IngredientRepository;
@@ -39,6 +40,8 @@ public class RecipeController {
         datamodel.addAttribute("formModalHidden", true);
         datamodel.addAttribute("searchForm", new Recipe());
         datamodel.addAttribute("recipe", new Recipe());
+        datamodel.addAttribute("formRecipe", new Recipe());
+        datamodel.addAttribute("newRecipe", new Recipe());
         datamodel.addAttribute("allIngredients", ingredientRepository.findAll());
 
         return "homepage";
